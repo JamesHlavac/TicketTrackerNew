39fd185ff1471ce1320cd9f69b0a805578db84b5
JamesHlavac <n00183513@unf.edu>
2013-09-20 20:15:17 -0400

    Here's the new version of TicketTracker, implementing IOC

---
+++ TicketTrackerNew/TicketTracker/Models/ITicketDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NinjectMvc.Abstract
+{
+    public interface iTicket
+    {
+       int getId();
+       string getName();
+       string getType; 
+       void setId(int id);
+       void setName(string name);
+       void setType(string Type);
+    }
+}
